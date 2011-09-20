3d88aa233eb0d963cde541d3e2b853130b0125f3
Mark Erdmann <mark.erdmann@gmail.com>
2011-09-19 23:32:40 -0700

    try to debug heroku resque

--- app/jobs/runner.rb
+++ app/jobs/runner.rb
@@ -2,13 +2,17 @@ class Runner
   @queue = :runner
   
   def self.perform(task_id)
+    REDIS.set('p', 1)
     begin
+      REDIS.set('p', 1.5)
       task = Task.find(task_id)
+      REDIS.set('p', 2)
       return unless task.running
       agent = Mechanize.new
       page = agent.get(task.url + "?key=#{task.api_key}")
       selector = task.css_selector
       values = page.search(selector).map {|el| el.text }
+      REDIS.set('p', 3)
       if task.extract_number
         values.map! {|v| v.scan(/\d+/)[0].to_i }
       end
@@ -16,7 +20,9 @@ class Runner
       if !values.empty?
         TaskMailer.alert_email(task, values).deliver
       end
+      REDIS.set('p', 4)
       Resque.enqueue_in(task.frequency.to_f.hours, Runner, task_id)
+      REDIS.set('p', 5)
     rescue => e
       REDIS.set('error', [e.inspect, e.backtrace].to_json)
     end
